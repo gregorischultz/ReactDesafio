8f6716da3d6b29d13044d5932d5434a0abf9a822
Gregori Schultz <gregorischultz07@gmail.com>
2023-12-11 18:58:23 +0100

    ChallengeReactBasics02

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function PokemonCard (){
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/> <figcaption>Bulbasaur</figcaption>
+        </figure>
+    );
+}
