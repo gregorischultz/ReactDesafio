2504291902564b971886b9e87a854f4adedfe9be
Gregori Schultz <gregorischultz07@gmail.com>
2023-12-13 14:03:14 +0100

    ChallengeReactBasics3

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import React from 'react';
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  return 
-  (<div>
-<PokemonCard/>
-  </div>)
+  return (
+    <div>
+      <PokemonCard/>
+    </div>
+  )
+  
 }
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    }
+]
+
+
+
+
 function PokemonCard (){
+    const pokemon = pokemonList[0];
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/> <figcaption>Bulbasaur</figcaption>
-        </figure>
-    );
+        <div>
+            <h2>{pokemon.name}</h2>
+            {pokemon.imgSrc ? (
+                <img src = {pokemon.imgSrc} alt = {pokemon.name}/>)
+                : (<p>???</p>)
+            }
+        </div>);
+    
 }
+export default PokemonCard;
