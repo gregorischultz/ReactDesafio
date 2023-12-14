6ede3c7f4327cdf42ad9b3a0b8ff356071c27b46
Gregori Schultz <gregorischultz07@gmail.com>
2023-12-14 12:32:34 +0100

    ChallengeReactBasics05

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 
 
@@ -18,6 +19,12 @@ function PokemonCard (props){
             }
         </div>);
          
+}
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name:PropTypes.string.isRequired,
+        imgSrc:PropTypes.string,
+    }).isRequired
 }
 
 export default PokemonCard;
