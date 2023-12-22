5ef568956cf24c55f412d43d46a910090ce1bcaa
Gregori Schultz <gregorischultz07@gmail.com>
2023-12-22 14:10:07 +0100

    Challenge Reac Basics 06

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,46 @@ import PokemonCard from "./components/PokemonCard";
 const pokemonList = [
   {
       name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
-  }
-]
+    },
+  ];
 
 function App() {
-  const selectedPokemon = pokemonList[0];
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  
+  const pokemonPrecedent = () => {
+    setpokemonIndex (prevIndex => prevIndex > 0 ? prevIndex - 1 : pokemonList.length - 1);
+  }
+
+  const pokemonSuivant = () => {
+    setpokemonIndex (prevIndex => prevIndex < pokemonList.length - 1 ? prevIndex + 1 : 0);
+  }
+
+  const selectedPokemon = pokemonList[pokemonIndex];
   return (
     <div>
-      <PokemonCard pokemon={selectedPokemon}/>
+      <PokemonCard pokemon={selectedPokemon} />
+      <button onClick={pokemonPrecedent}>Precedent</button> 
+      <button onClick={pokemonSuivant}>Suivant</button>
     </div>
   )
   
